e9b7cfa5a0886fcdf7176a2e6c438be79b400772
王鹏程 <wangpengcheng@ehomepay.com.cn>
2021-05-02 20:32:19 +0800

    add:1 of the 2nd week

---
+++ 01jvm/Hello.java
@@ -0,0 +1,11 @@
+package saturnStroller.jvm;
+
+/**
+ * @Description  自定义ClassLoader测试使用
+ * @Author SaturnStroller
+ */
+public class Hello {
+    static {
+        System.out.println("Hello,My ClassLoader!");
+    }
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ### 训练营作业
 
-#### 01jvm -- 第一周作业
+#### 01jvm 
 
-- 第2题	HelloClassLoader.java
-- 第3题	3-JVM内存相关启动参数关系图.png
+- 第一周作业	第2题（自定义ClassLoader）	HelloClassLoader.java
+- 第一周作业	第3题（JVM启动参数关系）	3-JVM内存相关启动参数关系图.png
